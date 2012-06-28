4e35fa29f61d1549033862a8f5ece0d193195a92
angel ignacio colmenares laguado <angel.ignacio.colmenares@gmail.com>
2012-06-27 19:39:49 -0500

    hacer funcionar https

---
+++ src/CacheTest/Test.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace CacheTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase ()
+        {
+        }
+    }
+}
+
